c8d4ba6751882c45ae20643411d7559fcac8adbe
filmbaz7 <farzadomidvar76@gmail.com>
2025-05-24 15:22:12 -0700

    Update run_spider.py

--- run_spider.py
+++ run_spider.py
@@ -1,62 +1,32 @@
-import os
-import sys
-import django
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-
 from jdsports_spider import JDSportsSpider
-from db_helper import add_discount
+from db_helper import save_discounts
+from twisted.internet import reactor
+from crochet import setup, wait_for
+setup()
 
-class CustomJDSportsSpider(JDSportsSpider):
-    def parse(self, response):
-        print(">>> Spider parsing...")
-        for product in response.css("span.itemContainer"):
-            name = product.css("span.itemTitle a::text").get()
-            priceWasText = product.css("span.was span::text").get()
-            priceIsText = product.css("span.now span::text").get()
-
-            if not priceWasText or not priceIsText:
-                continue
-
-            try:
-                priceWas = float(priceWasText.replace("€", "").replace(",", ".").strip())
-                priceIs = float(priceIsText.replace("€", "").replace(",", ".").strip())
-            except ValueError:
-                continue
-
-            discount = round((priceWas - priceIs) * 100 / priceWas, 0)
-            difference = priceWas - priceIs
-            link = response.urljoin(product.css("a.itemImage").attrib.get("href", ""))
-            image = response.urljoin(product.css("img.thumbnail").attrib.get("src", ""))
-
-            item = {
-                "name": name,
-                "priceWas": priceWas,
-                "priceIs": priceIs,
-                "difference": difference,
-                "discount": discount,
-                "link": link,
-                "image": image,
-            }
-
-            print(f">>> Item: {item}")
-            add_discount(name, link, priceWas, priceIs, discount, image)
-            yield item
-
-        next_page = response.css("a.btn.btn-default.pageNav[rel='next']::attr(href)").get()
-        if next_page:
-            yield response.follow(next_page, self.parse)
-
-
+@wait_for(timeout=60.0)
 def run_spider():
-    print(">>> Spider started...")
+    items_collected = []
+
+    def handle_result(item, response, spider):
+        items_collected.append(item)
+
     process = CrawlerProcess(settings={
-        "LOG_LEVEL": "ERROR",
+        "LOG_ENABLED": False,
+        "ITEM_PIPELINES": {},
     })
-    process.crawl(CustomJDSportsSpider)
+
+    crawler = process.create_crawler(JDSportsSpider)
+    crawler.signals.connect(handle_result, signal=scrapy.signals.item_scraped)
+    process.crawl(crawler)
     process.start()
-    print(">>> Spider finished.")
 
+    return items_collected
 
-if __name__ == "__main__":
-    run_spider()
+if __name__ == '__main__':
+    print(">>> Spider started...")
+    items = run_spider()
+    print(f">>> Found {len(items)} items")
+    save_discounts(items)
