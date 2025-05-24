7213c1ed06c7c91fd5d9cc5341763a9f9fa37a69
filmbaz7 <farzadomidvar76@gmail.com>
2025-05-24 16:00:04 -0700

    Update jdsports_spider.py

--- jdsports_spider.py
+++ jdsports_spider.py
@@ -5,36 +5,38 @@ class JDSportsSpider(scrapy.Spider):
     start_urls = ["https://www.jdsports.it/saldi/"]
 
     def parse(self, response):
-        products = response.css("div.productListItem")
-        for product in products:
-            name = product.css("div.itemTitle a::text").get()
-            price_was_text = product.css("div.was span::text").get()
-            price_is_text = product.css("div.now span::text").get()
+        # هر محصول داخل این div هاست
+        for product in response.css("div.itemContainer"):
+            name = product.css("span.itemTitle a::text").get()
 
-            if not price_was_text or not price_is_text:
+            price_was_text = product.css("span.was span[data-oi-price]::text").get()
+            price_now_text = product.css("span.now span[data-oi-price]::text").get()
+            discount_text = product.css("span.sav::text").get()
+
+            if not price_was_text or not price_now_text:
                 continue
 
             try:
                 price_was = float(price_was_text.replace("€", "").replace(",", ".").strip())
-                price_is = float(price_is_text.replace("€", "").replace(",", ".").strip())
+                price_now = float(price_now_text.replace("€", "").replace(",", ".").strip())
             except ValueError:
                 continue
 
-            discount = round((price_was - price_is) * 100 / price_was, 0)
-            difference = price_was - price_is
-            link = response.urljoin(product.css("a.itemImage::attr(href)").get())
-            image = response.urljoin(product.css("img.thumbnail::attr(src)").get())
+            discount = discount_text.strip() if discount_text else ""
+
+            link = response.urljoin(product.css("a.itemImage::attr(href)").get() or "")
+            image = response.urljoin(product.css("img.thumbnail::attr(src)").get() or "")
 
             yield {
                 "name": name,
                 "priceWas": price_was,
-                "priceIs": price_is,
-                "difference": difference,
+                "priceNow": price_now,
                 "discount": discount,
                 "link": link,
                 "image": image,
             }
 
+        # دنبال صفحه بعد بگرد
         next_page = response.css("a.btn.btn-default.pageNav[rel='next']::attr(href)").get()
         if next_page:
             yield response.follow(next_page, self.parse)
