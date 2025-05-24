5132983ffd6f791dc2a200f2342ab38d662ed202
filmbaz7 <farzadomidvar76@gmail.com>
2025-05-24 09:29:54 -0700

    Update scraper_runner.py

--- scraper_runner.py
+++ scraper_runner.py
@@ -1,6 +1,6 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from jdsports_spider import JDSportsSpider
+from jdsports.jdsports_spider import JDSportsSpider  # فرض بر این است که داخل فولدر jdsports هست
 
 items = []
 
@@ -9,8 +9,11 @@ class CustomSpider(JDSportsSpider):
         for item in super().parse(response):
             if item["discount"] >= 30:
                 items.append(item)
+                yield item  # مهم برای ذخیره آیتم‌ها توسط Scrapy
 
 def run_spider():
+    global items
+    items = []  # ریست قبل از اجرای هر بار
     process = CrawlerProcess(get_project_settings())
     process.crawl(CustomSpider)
     process.start()
