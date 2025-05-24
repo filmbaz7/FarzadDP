238885b27c8df32ad17d96ca8a43bd39605380c7
filmbaz7 <farzadomidvar76@gmail.com>
2025-05-24 10:28:04 -0700

    Update scraper_runner.py

--- scraper_runner.py
+++ scraper_runner.py
@@ -1,27 +1,25 @@
-from twisted.internet import reactor, defer
-from scrapy.crawler import CrawlerRunner
-from scrapy.utils.log import configure_logging
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 from jdsports_spider import JDSportsSpider
 
 items = []
 
-class CustomSpider(JDSportsSpider):
-    def parse(self, response):
-        for item in super().parse(response):
-            if item["discount"] >= 30:
-                items.append(item)
-                yield item
+class FilteredSpider(JDSportsSpider):
+    name = "filtered_spider"
 
-@defer.inlineCallbacks
-def crawl():
-    configure_logging()
-    runner = CrawlerRunner()
-    yield runner.crawl(CustomSpider)
-    reactor.stop()
+    def parse(self, response):
+        for result in super().parse(response):
+            if isinstance(result, dict) and result.get("discount", 0) >= 30:
+                items.append(result)
+                yield result
+            else:
+                yield result  # مثل Request برای صفحات بعد
 
 def run_spider():
     global items
     items = []
-    crawl()
-    reactor.run()
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(FilteredSpider)
+    process.start()
     return items
