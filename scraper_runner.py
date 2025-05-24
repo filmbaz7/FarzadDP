627e8a3876c33edb5c3bea2b9dc1cce1e94e380d
filmbaz7 <farzadomidvar76@gmail.com>
2025-05-24 12:15:01 -0700

    Update scraper_runner.py

--- scraper_runner.py
+++ scraper_runner.py
@@ -1,26 +1,21 @@
-import scrapy
 from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from jdsports_spider import JDSportsSpider
-
-items = []
-
-class FilteredSpider(JDSportsSpider):
-    name = "filtered_jdsports"
-
-    def parse(self, response):
-        # اگر parse اصلی generator برمی‌گردونه، باید ازش استفاده کنیم
-        for result in super().parse(response):
-            if isinstance(result, dict):
-                if result.get("discount", 0) >= 30:
-                    items.append(result)
-                    yield result
-                # اگر لازم نیست چیز دیگه‌ای yield کنیم، می‌تونیم ردش کنیم
+from jdsports_spider import JDSpider
+from db_helper import save_discount
 
 def run_spider():
-    global items
-    items = []
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(FilteredSpider)
-    process.start(stop_after_crawl=True)  # برای اجرای درست در سرور
-    return items
+    process = CrawlerProcess(settings={
+        'LOG_ENABLED': False,
+        'FEEDS': {
+            'items.json': {'format': 'json'},
+        }
+    })
+
+    class CustomSpider(JDSpider):
+        def parse(self, response):
+            # فرض کن اسپایدر تخفیف‌ها رو می‌گیره و اینجا ذخیره می‌کنیم
+            for item in super().parse(response):
+                save_discount(item['title'], item['link'])
+            yield from super().parse(response)
+
+    process.crawl(CustomSpider)
+    process.start(stop_after_crawl=True)
