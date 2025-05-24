0dfda8979bf4b466d66606e9da87418d37db0e2a
filmbaz7 <farzadomidvar76@gmail.com>
2025-05-24 13:25:38 -0700

    Update scraper_runner.py

--- scraper_runner.py
+++ scraper_runner.py
@@ -1,23 +1,28 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils import ossignal  # ← این خط اضافه شده
+import asyncio
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.project import get_project_settings
+from twisted.internet import asyncioreactor
 from jdsports_spider import JDSportsSpider
 from db_helper import save_discount
 
+# حتما باید اول ری‌اکتور asyncio ست بشه
+asyncioreactor.install()
+
 def run_spider():
-    ossignal.install_shutdown_handlers = lambda *a, **kw: None  # ← این خط اضافه شده
+    async def crawl():
+        runner = CrawlerRunner(settings={
+            'LOG_ENABLED': False,
+            'FEEDS': {
+                'items.json': {'format': 'json'},
+            }
+        })
 
-    process = CrawlerProcess(settings={
-        'LOG_ENABLED': False,
-        'FEEDS': {
-            'items.json': {'format': 'json'},
-        }
-    })
+        class CustomSpider(JDSportsSpider):
+            def parse(self, response):
+                for item in super().parse(response):
+                    save_discount(item['title'], item['link'])
+                    yield item
 
-    class CustomSpider(JDSportsSpider):
-        def parse(self, response):
-            for item in super().parse(response):
-                save_discount(item['title'], item['link'])
-            yield from super().parse(response)
+        await runner.crawl(CustomSpider)
 
-    process.crawl(CustomSpider)
-    process.start(stop_after_crawl=True)
+    asyncio.run(crawl())
